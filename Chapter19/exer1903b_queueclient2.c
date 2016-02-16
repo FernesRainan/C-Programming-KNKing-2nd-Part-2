a4747e0eea2f34af90674157d9479f5ff084b39d
FernesRainan <fernes.rainan@gmail.com>
2016-02-17 00:07:47 +0900

    Create exer1903b_queueclient2.c

---
+++ Chapter19/exer1903b_queueclient2.c
@@ -0,0 +1,34 @@
+// exercise 1903a array type queue test program
+
+#include <stdio.h>
+#include "queue.h"
+#include "queue2.c"
+
+int main (void)
+{
+	int i;
+	
+	insert(3);
+	insert(4);
+	insert(60);
+	insert(80);
+	
+	for(i = 0; i < 4; i++) {
+		printf("the value of queue is %d\n", qremove());
+	}
+	
+	for(i = 0; i < 100; i++) {
+		insert(i * 2);
+	}
+	
+	printf("The first value is %d, The last value is %d\n", first(), last());
+	
+	printf("Remove queue %d\n", qremove());
+	
+	insert(2);
+	insert(4);     //checking error status ==> meaningless
+	
+	return 0;
+}
+	
+
