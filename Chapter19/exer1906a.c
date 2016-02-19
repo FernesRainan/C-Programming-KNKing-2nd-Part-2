757154ade4eedfe64195d911fdfc47af6fc943af
FernesRainan <fernes.rainan@gmail.com>
2016-02-19 21:20:41 +0900

    Update exer1906a.c

--- Chapter19/exer1906a.c
+++ Chapter19/exer1906a.c
@@ -1,5 +1,5 @@
 // (a) Add a peek function to stackADT.c. This function will have a parameter of type
-// Stack. When called, it returns the top tiem on the stack but doesn't modify the stack. 
+// Stack. When called, it returns the top item on the stack but doesn't modify the stack. 
 
 #include <stdio.h>
 #include <stdlib.h>
