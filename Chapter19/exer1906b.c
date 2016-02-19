27c1bf282fbfdb050f3f4cfc0c80e43f7893ef32
FernesRainan <fernes.rainan@gmail.com>
2016-02-19 20:45:21 +0900

    Create exer1906b.c

---
+++ Chapter19/exer1906b.c
@@ -0,0 +1,83 @@
+// (b) Add a peek function to stackADT2.c. This function will have a parameter of type
+// Stack. When called, it returns the top tiem on the stack but doesn't modify the stack. 
+
+// The create function now calls malloc twice: once to allocate a stack_type
+// structure and once to allocate the array that will contain the stack items. Either call
+// of malloc could fail, causing terminated to be called. The destroy function
+// must call free twice to release all the memory allocated by create.
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "stackADT2.h"
+
+struct stack_type {
+	Item *contents;
+	int top;
+	int size;
+};
+
+static void terminate(const char *message)
+{
+	printf("%s\n", message);
+	exit(EXIT_FAILURE);
+}
+
+Stack create(int size)
+{
+	Stack s = malloc(sizeof(struct stack_type));
+	if(s == NULL)
+		terminate("Error in create: stack could not be created.");
+	s->contents = malloc(size * sizeof(Item));
+	if (s->contents == NULL) {
+		free(s);
+		terminate("Error in create: stack could not be created.");
+	}
+	s->top = 0;
+	s->size = size;
+	return s;
+}
+
+void destroy(Stack s)
+{
+	free(s->contents);
+	free(s);
+}
+
+void make_empty(Stack s)
+{
+	s->top = 0;
+}
+
+bool is_empty(Stack s)
+{
+	return s->top == 0;
+}
+
+bool is_full(Stack s)
+{
+	return s->top == s->size;
+}
+
+void push(Stack s, Item i)
+{
+	if (is_full(s))
+		terminate("Error in push: stack is full");
+	s->contents[s->top++] = i;
+}
+
+Item pop(Stack s)
+{
+	if (is_empty(s))
+		terminate("Error in pop: stack is empty.");
+	return s->contents[--s->top];
+}
+
+Item peek(const Stack s)
+{
+	if (is_empty(s))
+		terminate("Error in pop: stack is empty.");
+	int value = s->contents[--s->top];
+	s->contents[s->top++];
+	
+	return value;
+}
