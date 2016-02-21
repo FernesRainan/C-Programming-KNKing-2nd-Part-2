b8d4dae9cc9b16a97d03be8fc066a8a88efbb777
FernesRainan <fernes.rainan@gmail.com>
2016-02-21 20:49:56 +0900

    Create project1907_queueADT.h

---
+++ Chapter19/project1907_queueADT.h
@@ -0,0 +1,32 @@
+// A queue is similar to a stack, except that items are added at one end but removed from
+// the other in a FIFO fashion. Operations on a queue might include:
+
+// Inserting an item at the end of the queue
+// Removing an item from the beginning of the queue
+// Returning the first item in the queue (without changing the queue)
+// Returning the last item in the queue (without changing the queue)
+// Testing whether the queue is empty
+
+// Write an interface for a queue module in the form of a header file named queue.h
+
+#ifndef QUEUEADT_H
+#define QUEUEADT_H
+
+#include <stdbool.h>
+
+typedef int Item;
+typedef struct queue_type *Queue;
+
+static void terminate(const char *message);
+Queue create(void);
+void destroy(Queue q);
+void make_empty(Queue q);
+bool is_empty(Queue q);
+void insert(Queue q, Item i);
+Item qremove(Queue q);
+Item first(Queue q);
+Item last(Queue q);
+Item length(Queue q);
+
+#endif
+
