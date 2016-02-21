6c9c4d1dbfde8763f54dac4b866d3cdda0161f06
FernesRainan <fernes.rainan@gmail.com>
2016-02-21 16:11:55 +0900

    Create project1905_queueclient.c

---
+++ Chapter19/project1905_queueclient.c
@@ -0,0 +1,35 @@
+// exercise 1903a array type queue test program
+
+#include <stdio.h>
+#include "queueADT.h"
+
+int main (void)
+{
+	Queue q1;
+	int i;
+	
+	q1 = create();
+	
+	insert(q1, 3);
+	insert(q1, 4);
+	insert(q1, 60);
+	insert(q1, 80);
+	
+	for(i = 0; i < 4; i++) {
+		printf("the value of queue is %d\n", qremove(q1));
+	}
+	
+	for(i = 0; i < 100; i++) {
+		insert(q1, i * 2);
+	}
+	
+	printf("The first value is %d, The last value is %d\n", first(q1), last(q1));
+	printf("Remove queue %d\n", qremove(q1));
+	printf("Memeber's number of Queue is %d.\n", length(q1));
+	
+	destroy(q1);
+	
+	return 0;
+}
+	
+
