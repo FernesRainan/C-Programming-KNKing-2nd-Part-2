7704addbd2bdcf7679da9610458be249bac3cbd8
FernesRainan <fernes.rainan@gmail.com>
2016-05-14 22:49:05 +0900

    Create exer2309.c

---
+++ Chapter23/exer2309.c
@@ -0,0 +1,10 @@
+// Replace the test condition in the following if statement by a single call of strchr:
+// if (ch == 'a' || ch == 'b' || ch == 'c') ...
+
+char ch;
+
+if (strchr("abc", ch) != NULL)...
+	
+	
+	
+	
