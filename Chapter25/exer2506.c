6c0f5049c12a06da832fc556adbb1b1a04b22954
FernesRainan <fernes.rainan@gmail.com>
2016-06-13 21:19:49 +0900

    Create exer2506.c

---
+++ Chapter25/exer2506.c
@@ -0,0 +1,21 @@
+// Modify the following program fragment by replacing as many characters as possible by
+// trigraphs
+
+// while ((orig_char = getchar()) != EOF) {
+// 	new_char = orig_char ^ KEY;
+// 	if (isprint(orig_char) && isprint(new_char))
+// 		putchar(new_char);
+// 	else
+// 		putchar(orig_char);
+// 
+// }
+
+	while ((orig_char = getchar()) != EOF) ??<
+		new_char = orig_char ??' KEY;
+		if (isprint(orig_char) && isprint(new_char))
+			putchar(new_char);
+		else
+			putchar(orig_char);
+	??>
+	
+	
