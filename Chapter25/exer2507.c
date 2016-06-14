97263369cc0eaa061fee866b4f557ba5357c3941
FernesRainan <fernes.rainan@gmail.com>
2016-06-14 21:49:45 +0900

    Create exer2507.c

---
+++ Chapter25/exer2507.c
@@ -0,0 +1,19 @@
+// Modify the program fragment in Exercise 6 by replacing as many tokens as possible
+// by digraphs and macros defined in <iso646.h>
+
+// while ((orig_char = getchar()) != EOF) {
+// 	new_char = orig_char ^ KEY;
+// 	if (isprint(orig_char) && isprint(new_char))
+// 		putchar(new_char);
+// 	else
+// 		putchar(orig_char);
+// }
+
+ while ((orig_char = getchar()) not_eq EOF) <%
+ 	new_char = orig_char xor KEY;
+ 	if (isprint(orig_char) and isprint(new_char))
+ 		putchar(new_char);
+ 	else
+ 		putchar(orig_char);
+ %>
+	
