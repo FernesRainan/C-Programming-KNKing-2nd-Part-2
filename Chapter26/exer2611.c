3dc75a3856803eb22244b087f87b25d2920db649
FernesRainan <fernes.rainan@gmail.com>
2016-07-07 16:05:06 +0900

    Create exer2611.c

---
+++ Chapter26/exer2611.c
@@ -0,0 +1,4 @@
+// Although the bsearch function is normally used with a sorted array, it will sometimes
+// work correctly with an array that is only partially sorted. What condition must an array satisfy
+// to guarantee that bsearch works properly for a paricular key?
+// Hint: The answer appears in the C standard. 
